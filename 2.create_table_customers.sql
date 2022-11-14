a1c962db61eb875744ba68dc964a28f9f10a8fdb
asyamfaiq <asfaiq26@gmail.com>
2022-11-14 18:35:55 +0700

    testing

---
+++ 2.create_table_customers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `customers` (
+    `id` bigint(20) AUTO_INCREMENT NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `address` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `phone_number` varchar(255) NOT NULL,
+    'created_at' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ('id')
+);
